a6d9105a5c6f1462e522144714ddd2117fdc1d04
oyeyemi toheeb <opeyemioyeyemi141@gmail.com>
2024-01-17 23:07:20 +0100

    ip finder

--- Ip finder/src/FindIP.java
+++ Ip finder/src/FindIP.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class FindIP {
     public static void main(String[] args){
@@ -18,13 +22,31 @@ public class FindIP {
         JTextField urlTextField = new JTextField();
         urlTextField.setBounds(50,70, 200, 20 );
 
-
-        //
-        JButton findButton = new JButton("Find");
-        findButton.setBounds(30, 70, 50, 50);
+        JButton findButton = getFindButton(urlTextField, jFrame);
 
         jFrame.add(urlTextField);
         jFrame.add(urlLabel);
         jFrame.add(findButton);
+    }
+
+    private static JButton getFindButton(JTextField urlTextField, JFrame jFrame) {
+        JButton findButton = new JButton("Find");
+        findButton.setBounds(30, 70, 50, 50);
+
+        findButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String url = urlTextField.getText();
+
+                try{
+                    InetAddress inetAddress = InetAddress.getByName(url);
+                    String ip = inetAddress.getHostAddress();
+                    JOptionPane.showMessageDialog(jFrame,ip);
+                } catch (UnknownHostException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        return findButton;
     }
 }
